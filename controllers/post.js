2f29731ce018839bb398a551e681c24d4f97a4d0
Oxana Theis <otheis@protonmail.com>
2023-03-07 17:39:33 +0000

    change modifyPost controller with admin authorisation

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,7 @@
 const cloudinary = require("../cloudinary");
 const connection = require("../mysql");
+const dotenv = require("dotenv");
+dotenv.config();
 
 exports.createPost = async (req, res, next) => {
   //how set token in postman, see only req.auth.userId object  without token in response
@@ -31,55 +33,59 @@ exports.createPost = async (req, res, next) => {
     }
   });
 };
-
 exports.modifyPost = async (req, res, next) => {
-  if (req.file) {
-    //check to modify query on  req.params.id
-    const sqlFindPost = "SELECT * FROM post WHERE id_post = '" + req.body.id_post + "' ";
-    connection.query(sqlFindPost, async (err, result) => {
-      //cancel media from Cloudinary
-      await cloudinary.uploader.destroy(result[0].cloudinary_id);
-    });
-    //upload new media to Cloudinary
-    let media = await cloudinary.uploader.upload(req.file.path);
-    //COALESCE update title, body if not null, otherwise returns existing value
-    const sqlUpdatePost = "UPDATE post SET title = COALESCE(?, title),body= COALESCE(?, body),image_url=?,cloudinary_id=? WHERE id_post = '" + req.body.id_post + "' ";
-    connection.query(sqlUpdatePost, [req.body.title, req.body.body, media.secure_url, media.public_id], (err, result) => {
-      if (!err) {
-        res.json({
-          message: "Post modified with file",
-        });
-      } else {
-        res.json({
-          error: true,
-          message: err,
-        });
-      }
-    });
-  } else {
-    //check to modify query on  req.params.id
-    //COALESCE update title, body if not null, otherwise returns existing value
-    const sqlUpdatePost = "UPDATE post SET title = COALESCE(?, title),body= COALESCE(?, body) WHERE id_post = '" + req.body.id_post + "' ";
-    connection.query(sqlUpdatePost, [req.body.title, req.body.body], (err, result) => {
-      if (!err) {
-        res.json({
-          message: "Post modified",
-        });
-      } else {
-        res.json({
-          error: true,
-          message: err,
+  //check to modify query on  req.params.id
+  const sqlFindPost = "SELECT * FROM post WHERE id_post = '" + req.body.id_post + "' ";
+  connection.query(sqlFindPost, async (err, result) => {
+    //check the id of user or admin
+    if (result[0].id_user === req.auth.userId || req.auth.userId === 18) {
+      //check if there is a new file added
+      if (req.file) {
+        //cancel media from Cloudinary
+        await cloudinary.uploader.destroy(result[0].cloudinary_id);
+        //upload new media to Cloudinary
+        let media = await cloudinary.uploader.upload(req.file.path);
+
+        //COALESCE update title, body if not null, otherwise returns existing value
+        const sqlUpdatePost = "UPDATE post SET title = COALESCE(?, title),body= COALESCE(?, body),image_url=?,cloudinary_id=? WHERE id_post = '" + req.body.id_post + "' ";
+
+        connection.query(sqlUpdatePost, [req.body.title, req.body.body, media.secure_url, media.public_id], (err, result) => {
+          res.json({
+            message: "Post modified with file",
+          });
         });
       }
-    });
-  }
+      //if there isn't new file update with req.body values
+      else {
+        const sqlUpdatePost = "UPDATE post SET title = COALESCE(?, title),body= COALESCE(?, body) WHERE id_post = '" + req.body.id_post + "' ";
+        connection.query(sqlUpdatePost, [req.body.title, req.body.body], (err, result) => {
+          if (!err) {
+            res.json({
+              message: "Post modified",
+            });
+          } else {
+            res.json({
+              message: "Non authorized",
+            });
+          }
+        });
+      }
+    }
+    //refuse connection if not owner of the post or admin
+    else {
+      res.json({
+        message: "Non authorized",
+      });
+    }
+  });
 };
+
 exports.deletePost = (req, res, next) => {
   const sqlFindPost = "SELECT * FROM post WHERE id_post = '" + req.params.id + "' ";
   connection.query(sqlFindPost, async (err, result) => {
     //check if the user is a owner of the Post object or admin wit id 18
     //compare the id of user in DB with req.auth.userId (received once user is logged )
-  
+
     if (result[0].id_user === req.auth.userId || req.auth.userId === 18) {
       //Delete image from cloudinary
       cloudinary.uploader.destroy(result[0].cloudinary_id);
@@ -96,13 +102,11 @@ exports.deletePost = (req, res, next) => {
           });
         }
       });
-    }else{
+    } else {
       res.status(401).json({ message: "Not authorized" });
     }
   });
 };
-
-
 
 exports.getOnePost = async (req, res, next) => {
   const sqlFindPost = "SELECT * FROM post WHERE id_post = '" + req.params.id + "' ";
