31cbadd95379e9ea830a6ac3a22f52c468aa71f3
Oxana Theis <otheis@protonmail.com>
2023-10-02 12:29:58 +0100

    add reset password controller

--- controllers/user.js
+++ controllers/user.js
@@ -21,6 +21,7 @@ exports.signup = (req, res, next) => {
     } else if (req.body.password.length < 4 || req.body.password.length > 24) {
       message.password = "Password must be between 4 and 24 characters";
     }
+
     return message;
   }
 
@@ -150,7 +151,7 @@ exports.forgotPassword = (req, res, next) => {
       if (!req.body.email) {
         message.error = "Please fill in email";
       } else {
-        message.error= "Incorrect credentials";
+        message.error = "Incorrect credentials";
       }
 
       return message;
@@ -167,13 +168,12 @@ exports.forgotPassword = (req, res, next) => {
       });
     }
     if (result.length > 0) {
-      console.log(result[0].email)
+      console.log(result[0].email);
       const recoveryToken = jwt.sign({}, "RANDOM_TOKEN_SECRET", { expiresIn: "5m" });
       // console.log(newToken)
       const sqlUpdateUserRecoveryToken = "UPDATE user SET recovery_token = COALESCE(?, recovery_token) WHERE email=?";
 
       connection.query(sqlUpdateUserRecoveryToken, [recoveryToken, req.body.email], (err, result) => {
-  
         if (err) throw err;
 
         const transporter = nodemailer.createTransport({
@@ -189,33 +189,79 @@ exports.forgotPassword = (req, res, next) => {
         const mailOptions = {
           from: process.env.EMAIL,
           // to: result[0].email,
-          to:req.body.email,
+          to: req.body.email,
           // to: "otheis@protonmail.com",
           subject: "Password Reset",
-          text: `Click on the following link to reset your password: http://localhost:3001/reset-password?token=${recoveryToken}`,
+          text: `Click on the following link to reset your password: http://localhost:3001/reset-password?token=${recoveryToken}.`,
         };
 
         transporter.sendMail(mailOptions, function (error, response) {
-          console.log(response)
+          console.log(response);
           if (error) {
             console.log("Error sending password reset email:", error);
           } else {
             console.log("Password reset email sent successfully");
-            
-            
           }
-          
-          
         });
- 
-
       });
 
       res.status(200).json({
         userId: result[0].id_user,
         // token: result[0].recovery_token
         token: recoveryToken,
-        message:"Password reset link sent successfully to your email "
+        error: false,
+        message: "Password reset link sent successfully to your email ",
+      });
+    }
+  });
+};
+
+exports.resetPassword = (req, res, next) => {
+  const authToken = req.query.token;
+  console.log(authToken);
+
+  const sqlCheckUserRecoveryToken = "SELECT * FROM user WHERE recovery_token = '" + authToken + "' ";
+
+  connection.query(sqlCheckUserRecoveryToken, (err, result) => {
+    // Function to validate fields and return error messages
+    function validateFields(req) {
+      let message = {};
+
+      if (!req.body.password) {
+        message.password = "Please fill in password";
+      }
+      // else {
+      // //   message.error = "Incorrect credentials";
+      // // }
+      return message;
+    }
+
+    let message = validateFields(req);
+
+    // console.log(result[0]);
+    // Check the row in the user table. Result is an array with object (id_user, email, password)
+    if (result.length === 0) {
+      res.json({
+        error: true,
+        message: message,
+      });
+    }
+    if (result.length > 0) {
+      let password = bcrypt.hashSync(req.body.password, 10);
+
+      const sqlUpdateUserPassword = "UPDATE user SET password = COALESCE(?, password) WHERE recovery_token=?";
+
+      connection.query(sqlUpdateUserPassword, [password, authToken], (err, result) => {
+        if (!err) {
+          res.json({
+            message: "Password modified",
+          });
+        } else {
+          res.json({
+            error: true,
+            message: err,
+          });
+        }
       });
     }
   });
